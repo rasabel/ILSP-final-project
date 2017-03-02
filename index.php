a648de719dedbd1cf8f050a8a6211bcd42439248
Ras Goal Gule <abelberhau21@gmail.com>
2017-03-02 15:11:24 -0700

    search form beta

--- index.php
+++ index.php
@@ -42,20 +42,41 @@
       border-bottom-right-radius: 14px;
 
     }
+    .search_title{
+      margin: auto;
+      text-align: center;
+      margin-top:100px;
+      background-color:#ffffff;
+      width: 150px;
+
+    }
     .search{
       margin: auto;
       text-align: center;
-      margin-top:150px;
+      margin-top:30px;
       background-color:#ffffff;
       width: 600px;
     }
+    .search_button{
+      margin: auto;
+      text-align: center;
+      margin-top:10px;
 
+      width: 600px;
+    }
 
+    .serch_form{
+      padding: 20px;
+    }
+    .catagories{
+      margin-top: 20px;
+    }
   </style>
   <head>
 
     <body>
       <div class="full_body w3-container w3-light-blue">
+        <!--the page header-->
         <div class=" w3-row">
             <div class="w3-col m3">
               <div class="logo">
@@ -69,11 +90,67 @@
               </div>
           </div>
         </div>
+
+          <!--the serch_form-->
         <div class="w3-row">
+          <div class="search_title">
+              <p class="w3-xlarge">Search:</p>
+          </div>
+
             <div class="search">
-                search box here
+
+              <ul class="w3-navbar">
+                 <li><a href="#" onclick="openCity('sea_place')">place</a></li>
+                 <li><a href="#" onclick="openCity('sea_name')">name</a></li>
+                 <li><a href="#" onclick="openCity('sea_service')">service</a></li>
+              </ul>
+              <div id="sea_place" class="sea_input">
+                <form class="serch_form">
+                  <label>search: </label>
+                  <input type="text" placeholder="enter the place name">
+                  <label>near: </label>
+                  <input type="text" placeholder="location">
+                </form>
+
+              </div>
+
+              <div id="sea_name" class="sea_input">
+                <form class="serch_form">
+                  <label>search: </label>
+                  <input type="text" placeholder="enter the name">
+                  <label>near: </label>
+                  <input type="text" placeholder="location">
+                </form>
+              </div>
+
+              <div id="sea_service" class="sea_input">
+                <form class="serch_form">
+                  <label>search: </label>
+                  <input type="text" placeholder="enter the service name">
+                  <label>near: </label>
+                  <input type="text" placeholder="location">
+                </form>
+
+            </div>
+            <div class="search_button">
+              <button class="w3-btn w3-round-large w3-right">Rounder</button>
             </div>
         </div>
+
+          <!--the catagories-->
+          
       </div>
+
+      <script>
+      openCity("sea_name");
+      function openCity(search_input) {
+        var i;
+        var x = document.getElementsByClassName("sea_input");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        document.getElementById(search_input).style.display = "block";
+      }
+      </script>
     </body>
 </html>
