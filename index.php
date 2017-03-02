82c68e3b7799219c09923d8d1a9189b80d3fdd76
Ras Goal Gule <abelberhau21@gmail.com>
2017-03-02 14:17:14 -0700

    set position of the header

--- index.php
+++ index.php
@@ -7,122 +7,73 @@
  */
 ?>
 
-<!DOCTYPE HTML>
+<!doctype hmtl>
 <html>
-<head>
-    <title>LISP</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="lib/w3.css">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="lib/jquery-3.1.1.js"></script>
+  <head>
+    <title>ILSP-finalproject</title>
 
-</head>
-<body>
-        <div class="first-padding w3-light-blue">
-            <header class="w3-row">
-                <div class="w3-col m1">
-                    head-left
-                </div>
-                <div class="w3-col m10">
-                    <ul class="w3-navbar ">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Link 1</a></li>
-                        <li><a href="#">Link 2</a></li>
-                        <li  class="w3-right"><a href="#" onclick="document.getElementById('id01').style.display='block'">Sign In / Sign up</a></li>
-                    </ul>
-                </div>
-                <div class="w3-col m1">
-                    head-right
-                </div>
-            </header>
+<link rel="stylesheet" type="text/css" href="lib/w3.css">
+  <style>
+
+    .full_body{
+      height: 100%;
+      width: 100%;
+      padding:0px;
+      background-image: url("img/background.jpg");
+      background-repeat: no-repeat;
+      background-size: 100%;
+    }
+    .myimage{
+      width: 100%;
+      height: 100%;
+
+    }
+    .logo{
+      background-color: red;
+      height: 80px;
+      width: 110px;
+      margin-left: 120px;
+
+    }
+    .auth{
+      margin-right: 120px;
+      padding: 10px;
+      border-bottom-left-radius: 14px;
+      border-bottom-right-radius: 14px;
+
+    }
+    .search{
+      margin: auto;
+      text-align: center;
+      margin-top:150px;
+      background-color:#ffffff;
+      width: 600px;
+    }
+
+
+  </style>
+  <head>
+
+    <body>
+      <div class="full_body w3-container w3-light-blue">
+        <div class=" w3-row">
+            <div class="w3-col m3">
+              <div class="logo">
+              </div>
+            </div>
+
+            <div class="w3-col m9 ">
+              <div class="auth w3-right w3-khaki">
+                <a href="#">signup</a> /
+                <a href="#">signin</a>
+              </div>
+          </div>
         </div>
-        <div class="second-padding">
-           <div class="w3-row">
-               <div class="w3-col m2 w3-blue">
-                   body-left
-               </div>
-                   <div class="w3-col m8">
-                       <div class="w3-row">
-                           body-main
-                       </div>
-                       <div class="w3-row">
-                           catagories list
-                       </div>
-                       <div class="w3-row w3-card w3-light-gray">
-                           <div class="w3-col m4 w3-card-4">popular list left</div>
-                           <div class="w3-col m4 w3-card-4">popular list center</div>
-                           <div class="w3-col m4 w3-card-4">popular list right</div>
-                       </div>
-                   </div>
-               <div class="w3-col m2 w3-blue">
-                   body-right
-               </div>
-           </div>
+        <div class="w3-row">
+            <div class="search">
+                search box here
+            </div>
         </div>
-        <div class="first-padding w3-light-blue">
-           <footer class="w3-container">
-               footer
-           </footer>
-        </div>
-
-<!--model for login screen-->
-         <div id="id01" class="w3-modal ">
-             <input type="hidden" value="1" id="loginActive" class="loginActive">
-             <span onclick="document.getElementById('id01').style.display='none'" class="w3-closebtn w3-hover-red w3-container w3-padding-16 w3-display-topright">×</span>
-             <div class="w3-modal-content">
-                 <div class="w3-card-4">
-                     <header class="w3-container w3-blue">
-                         <h1 id="page_name">LOG IN</h1>
-                     </header>
-
-                     <div class="w3-container w3-light-gray w3-padding-24 ">
-                         <label id="label1">User Name/Email:</label>
-                         <input id="userEmail" class="w3-input" type="text" placeholder="Enter name">
-
-                         <label id="label2">Password:</label>
-                         <input  class="w3-input" type="text" placeholder="how to change it in the java script">
-                         <!--how to change the 'placeholder' value by using java script-->
-                         <div class="login_option" id="login_option">
-                             <input class="w3-check" type="checkbox" checked="checked">
-                             <label class="w3-validate">Remember</label>
-                             <br>
-                             <span>Forget<a href="#">password?</a></span>
-                         </div>
-                     </div>
-
-                     <footer class="w3-container w3-blue w3-padding-16">
-                         <span><button class="w3-btn w3-lime" id="submit_button">LOG IN</button></span>
-                         <span><button class="w3-btn w3-lime" onclick="document.getElementById('id01').style.display='none'" >Cancel</button></span>
-                         <a class="w3-right" id="toggle_signup" href="#">Sign up</a>
-                     </footer>
-                 </div>
-             </div>
-        </div>
-
-<script>
-    $("#toggle_signup").click(function () {
-        if($("#loginActive").val()=="1"){
-            $("#loginActive").val("0");
-            $("#page_name").html("SIGN UP");
-            $("#label1").html("Organization Name:");
-            $("#label2").html("Email:");
-            $("#login_option").hide();
-            $("#submit_button").html("SIGN UP");
-            $("#toggle_signup").html("Log in");
-        }else {
-            $("#loginActive").val("1");
-            $("#page_name").html("LOG IN");
-            $("#label1").html("User name/Email:");
-            $("#label2").html("Password:");
-            $("#login_option").show();
-            $("#submit_button").html("LOG IN");
-            $("#toggle_signup").html("Sign up");
-        }
-    });
-    $("#submit_button").click(function () {
-       //alert("submited");
-        alert($("#userEmail").val(),$("#"));
-    });
-</script>
-</body>
+      </div>
+    </body>
 </html>
